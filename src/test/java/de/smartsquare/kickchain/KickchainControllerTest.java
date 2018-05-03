066c4d379819f40a9abc463e82a705d807a2fcc0
michaelende <michael.ende@arcor.de>
2018-05-03 22:21:07 +0200

    added new Tests for nodes/register

--- src/test/java/de/smartsquare/kickchain/KickchainControllerTest.java
+++ src/test/java/de/smartsquare/kickchain/KickchainControllerTest.java
@@ -1,5 +1,6 @@
 package de.smartsquare.kickchain;
 
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.smartsquare.kickchain.domain.KcGame;
 import de.smartsquare.kickchain.domain.KcScore;
@@ -11,7 +12,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
+import java.util.Set;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItem;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -36,5 +42,33 @@ public class KickchainControllerTest {
                 .content(mapper.writeValueAsString(game)))
                 .andExpect(status().isOk())
                 .andExpect(content().string("2"));
+    }
+
+    @Test
+    public void testRegister() throws Exception {
+        String node = "162.168.2.1:8080";
+        mapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, false);
+        MvcResult result = mvc.perform(post("/nodes/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(node))
+                .andExpect(status().isCreated()).andReturn();
+        Set<String> registeredNodes = mapper.readValue(result.getResponse().getContentAsString(), Set.class);
+
+        assertThat(registeredNodes, hasItem("localhost:8080"));
+    }
+
+    @Test
+    public void testRegister_fail() throws Exception {
+        mvc.perform(post("/nodes/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(""))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testResolve() throws Exception {
+      /*  mvc.perform(get("/nodes/resolve")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());*/
     }
 }
