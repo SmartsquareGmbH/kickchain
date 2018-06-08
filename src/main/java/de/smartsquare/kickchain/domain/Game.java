25e48a3053e386bb6c66059841196e499a129fef
Sascha Koch <koch@smartsquare.de>
2018-06-08 16:05:16 +0200

    game nonce removed

--- src/main/java/de/smartsquare/kickchain/domain/Game.java
+++ src/main/java/de/smartsquare/kickchain/domain/Game.java
@@ -4,7 +4,6 @@ import de.smartsquare.kickchain.BlockchainException;
 import de.smartsquare.kickchain.MessageDigestUtils;
 
 import java.security.NoSuchAlgorithmException;
-import java.time.Instant;
 import java.util.Objects;
 
 public class Game implements BlockContent {
@@ -13,9 +12,6 @@ public class Game implements BlockContent {
     private Team team2;
     private Score score;
 
-    private Instant created;
-    private String nonce;
-
     public Game() {
     }
 
@@ -23,8 +19,6 @@ public class Game implements BlockContent {
         this.team1 = team1;
         this.team2 = team2;
         this.score = score;
-        this.created = Instant.now();
-        this.nonce = String.format("%s%f", this.created, Math.random());
     }
 
     public Team getTeam1() {
@@ -41,7 +35,7 @@ public class Game implements BlockContent {
 
     @Override
     public String getTransactionId() {
-        String trxHash = String.format("%d-%d-%d-%s", team1.hashCode(), team2.hashCode(), score.hashCode(), created);
+        String trxHash = String.format("%d-%d-%d", team1.hashCode(), team2.hashCode(), score.hashCode());
         try {
             return MessageDigestUtils.sha256(trxHash);
         } catch (NoSuchAlgorithmException e) {
@@ -53,7 +47,7 @@ public class Game implements BlockContent {
     @Override
     public String getSignature() throws BlockchainException {
         try {
-            return MessageDigestUtils.sha256(String.format("%s%s", getTransactionId(), nonce));
+            return MessageDigestUtils.sha256(String.format("%s", getTransactionId()));
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
             throw new BlockchainException("Unable to get Signature for transaction " + getTransactionId());
@@ -67,14 +61,12 @@ public class Game implements BlockContent {
         Game game = (Game) o;
         return Objects.equals(team1, game.team1) &&
                 Objects.equals(team2, game.team2) &&
-                Objects.equals(score, game.score) &&
-                Objects.equals(created, game.created) &&
-                Objects.equals(nonce, game.nonce);
+                Objects.equals(score, game.score) ;
     }
 
     @Override
     public int hashCode() {
 
-        return Objects.hash(team1, team2, score, created, nonce);
+        return Objects.hash(team1, team2, score);
     }
 }
